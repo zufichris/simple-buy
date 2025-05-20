e13de4ce64fede0dafa4a99a9bda7e13e7847469
zufichris <zufichris@gmail.com>
2025-05-20 18:22:39 +0400

    feat/Set up swagger for API docs with @elysia/swagger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import router from "./routes";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .onAfterHandle(({ server, request, set, response, store, status }) => {
+    console.log(
+      `${request.method}/ ${set.status} IP:${server?.requestIP(request)?.address}`,
+    );
+  })
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: "Super Buy Documentation",
+          version: "1.0",
+          description:
+            "A simple e-comerce application to demonstrate popular  API infrastructures",
+          license: {
+            name: "MIT",
+          },
+        },
+      },
+    }),
+  )
+  .get("/", async () => {
+    return {
+      message: "Hello World",
+    };
+  })
+  .use(router)
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `Super Buy is running at ${app.server?.hostname}:${app.server?.port}`,
 );
